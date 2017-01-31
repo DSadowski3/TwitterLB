fccc9e9f43f28a5339c5e645f9bb4db6f9420920
DSadowski3 <dsadowski2001@Dominiks-MacBook-Pro.local>
2017-01-30 23:24:51 -0600

    Added text fields

--- TwitterLB/Cells.swift
+++ TwitterLB/Cells.swift
@@ -6,4 +6,83 @@
 //  Copyright © 2017 Dominik Sadowski. All rights reserved.
 //
 
-import Foundation
+import LBTAComponents
+
+
+class UserFooter: DatasourceCell {
+    override func setupViews() {
+        super.setupViews()
+        
+        backgroundColor = .red
+    }
+}
+
+class UserHeader: DatasourceCell {
+    override func setupViews() {
+        super.setupViews()
+        
+        backgroundColor = .blue
+    }
+}
+
+class UserCell: DatasourceCell {
+    
+    override var datasourceItem: Any? {
+        didSet {
+            nameLabel.text = datasourceItem as? String
+        }
+    }
+    
+    let profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Test Test Test"
+        label.backgroundColor = .green
+        return label
+    }()
+    
+    let userName: UILabel = {
+        let label = UILabel()
+        label.text = "username"
+        label.backgroundColor = .purple
+        return label
+    }()
+    
+    let bioText: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .yellow
+        return textView
+    }()
+    
+    let followBtn: UIButton = {
+        let followBtn = UIButton()
+        followBtn.backgroundColor = .cyan
+        return followBtn
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        addSubview(profileImage)
+        addSubview(nameLabel)
+        addSubview(userName)
+        addSubview(bioText)
+        addSubview(followBtn)
+        
+        profileImage.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
+        
+        
+        nameLabel.anchor(profileImage.topAnchor, left: profileImage.rightAnchor, bottom: nil, right: followBtn.leftAnchor, topConstant: 0, leftConstant: 8, bottomConstant: 0, rightConstant: 12, widthConstant: 0, heightConstant: 20)
+        
+        userName.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 8, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+        
+        bioText.anchor(userName.bottomAnchor, left: userName.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+        followBtn.anchor(topAnchor, left: nil, bottom: nil, right: self.rightAnchor, topConstant: 12, leftConstant: 0, bottomConstant: 0, rightConstant: 12, widthConstant: 120, heightConstant: 34)
+    }
+}
--- TwitterLB/HomeDatasourceController.swift
+++ TwitterLB/HomeDatasourceController.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2017 Dominik Sadowski. All rights reserved.
 //
 
-import Foundation
+import LBTAComponents
+
+class HomeDatasourceController: DatasourceController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let homeDatasource = HomeDatasource()
+        self.datasource = homeDatasource
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+}
